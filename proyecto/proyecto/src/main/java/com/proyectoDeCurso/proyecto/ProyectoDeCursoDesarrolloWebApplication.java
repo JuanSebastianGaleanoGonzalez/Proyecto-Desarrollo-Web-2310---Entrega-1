fc58c791753bf3a9c5e5f9513dce0d4af31f35e1
JuanSebastianGaleanoGonzalez <71532153+JuanSebastianGaleanoGonzalez@users.noreply.github.com>
2023-03-02 04:43:35 -0500

    Add files via upload

---
+++ proyecto/proyecto/src/main/java/com/proyectoDeCurso/proyecto/ProyectoDeCursoDesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoDeCurso.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDeCursoDesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDeCursoDesarrolloWebApplication.class, args);
+	}
+
+}
---
+++ proyecto/proyecto/src/test/java/com/proyectoDeCurso/proyecto/ProyectoDeCursoDesarrolloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoDeCurso.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDeCursoDesarrolloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
